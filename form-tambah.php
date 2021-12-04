d93370ee25b59122ee13629696b1cade03842544
Itsjohanes <71767003+Itsjohanes@users.noreply.github.com>
2021-12-04 14:11:04 +0700

    Update form-tambah.php

--- form-tambah.php
+++ form-tambah.php
@@ -81,49 +81,7 @@
         }
         
 
-        $target_dir = "upload/";
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-        $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $image_name	= addslashes($_FILES['fileToUpload']['name']);
-
-        $uploadOk = 1;
-        } else {
-        $errorupload =  "File bukan gambar.";
-        $uploadOk = 0;
-        }
-if (file_exists($target_file)) {
-    $errorupload = "File udh ada.";
-    $uploadOk = 0;
-  }
-  
-  // Check file size
-  if ($_FILES["fileToUpload"]["size"] > 1000000000) {
-    $errorupload = "Gambar terlalu besar .";
-    $uploadOk = 0;
-  }
-  
-  // Allow certain file formats
-  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-  && $imageFileType != "gif" ) {
-    $errorupload ="Hanya JPG, JPEG, PNG & GIF yang diizinkan.";
-    $uploadOk = 0;
-  }
-  
-  if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-  } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-      echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-    } else {
-        $errorupload = "Sorry, Terjadi error.";
-    }
-  }
-  
+        
 
         
             $sql_insert = "INSERT INTO mahasiswa (nim,nama,email,alamat,kelamin) VALUE ('$nim','$nama','$email','$alamat','$kelamin')";
